6decdbae075b5aaa959cbf3971493c2d852e240e
Rahul <bansalgippcy@gmail.com>
2025-08-29 12:07:25 +0530

    added code

--- third-maximum-number.c
+++ third-maximum-number.c
@@ -1,10 +1,7 @@
-<<<<<<< HEAD
 int thirdMax(int* nums, int numsSize) {
     long long first=LLONG_MIN;
     long long second=LLONG_MIN;
     long long third=LLONG_MIN;
-
-    
 
     for(int i=0;i<numsSize;i++){
         long long x=nums[i];
@@ -25,31 +22,3 @@ int thirdMax(int* nums, int numsSize) {
     return (third==LLONG_MIN)?(int)first:(int)third;
     
 }
-=======
-int thirdMax(int* nums, int numsSize) {
-    long long first=LLONG_MIN;
-    long long second=LLONG_MIN;
-    long long third=LLONG_MIN;
-
-    
-
-    for(int i=0;i<numsSize;i++){
-        long long x=nums[i];
-
-    if(x==first || x==second || x==third)  continue;
-
-    if(x>first){
-        third=second;
-        second=first;
-        first=x;
-    }else if(x>second){
-        third=second;
-        second=x;
-    }else if(x>third){
-        third=x;
-        }
-    }
-    return (third==LLONG_MIN)?(int)first:(int)third;
-    
-}
->>>>>>> 3a12b13235e62db151d7ffefc36abc2501b6336c
