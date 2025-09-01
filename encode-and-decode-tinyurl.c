f3dfa3d52af4fc688234c728cdf5a5be1a89fb55
Rahul <bansalgippcy@gmail.com>
2025-09-01 17:11:33 +0530

    added code

--- encode-and-decode-tinyurl.c
+++ encode-and-decode-tinyurl.c
@@ -1,11 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_URLS 10000
+#define BASE_URL "http://tinyurl.com/"
+
+char* url_db[MAX_URLS];
+int counter = 0;
+
 /** Encodes a URL to a shortened URL. */
 char* encode(char* longUrl) {
-    return longUrl;
+    url_db[counter]=strdup(longUrl);   // save original URL
+    char* tiny=(char*)malloc(100);
+    sprintf(tiny, "%s%d", BASE_URL, counter); // make short url
+    counter++;
+    return tiny; // caller must free
 }
 
 /** Decodes a shortened URL to its original URL. */
 char* decode(char* shortUrl) {
-    return shortUrl;
+    int id=atoi(strrchr(shortUrl, '/') + 1); // get number after last '/'
+    return url_db[id];
 }
 
 // Your functions will be called as such:
